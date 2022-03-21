dc7df0ade72a39ba51fee4e2fd3655e2f27df124
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-21 21:21:43 +0000

    Saindo da tela de splash em 2 segundos

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Image, StyleSheet} from 'react-native';
+import {useRouteActions} from '../features/route-context';
+import Routes from '../enums/Routes';
 
 export default function Splash() {
+  const {navigate} = useRouteActions();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate(Routes.Home);
+    }, 2000);
+  }, [navigate]);
+
   return (
     <View style={Styles.container}>
       <Image style={Styles.logo} source={require('../assets/logo-2.png')} />
