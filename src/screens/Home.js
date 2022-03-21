89a7bf287da1dfc99d7a3941e4fddf62e0e032ac
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-21 15:22:29 +0000

    Recuperando os dados da categoria

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ScrollView, View, Text, StyleSheet} from 'react-native';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
+const URL = 'https://blog.coursify.me/wp-json/wp/v2/categories/';
+
 export default function Home() {
+  const [categories, setCategories] = useState([]);
+
+  const loadData = async () => {
+    const reponse = await fetch(URL);
+    const json = await reponse.json();
+    console.log('data: ', json[0].name);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
-    <View>
+    <ScrollView>
       <Navbar />
-      <Text>Home</Text>
+      <View style={Styles.container}>
+        <Text>Home</Text>
+      </View>
       <Footer />
-    </View>
+    </ScrollView>
   );
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    height: 600,
+  },
+});
